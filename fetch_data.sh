4835c15d5f0624572d7f3130438d3a77481dae63
Daniel_Stoxreiter <daniel.stoxreiter@outlook.de>
2022-11-25 10:09:18 +0100

    updated micro editor to v2.2.2; changed fetch data workflow to include frd-entities

--- fetch_data.sh
+++ fetch_data.sh
@@ -16,9 +16,15 @@ rm master.zip
 for f in  $(find -path "./frd-data-main/werke/*/*.xml" | grep __); do
   b=$(basename $f); cp -v $f ./data/editions/plain/${b%}
 done
-mv ./frd-data-main/indices ./data
 
 rm -rf ./frd-data-main
+
+wget https://github.com/freud-digital/frd-entities/archive/refs/heads/main.zip
+unzip main
+mv ./frd-entities-main/out/*.xml ./data/indices
+
+rm main.zip
+rm -rf ./frd-entities-main
 
 wget https://github.com/freud-digital/frd-working-data/archive/refs/heads/main.zip
 unzip main
@@ -33,6 +39,6 @@ rm -rf ./frd-working-data-main
 add-attributes -g "./data/editions/critical/*.xml" -b "https://id.acdh.oeaw.ac.at/freud-hka"
 add-attributes -g "./data/indices/*.xml" -b "https://id.acdh.oeaw.ac.at/freud-hka"
 
-denormalize-indices -f "./data/editions/critical/*.xml" -i "./data/indices/*.xml" -m ".//*[@ref]/@ref | .//*/@source" -x ".//tei:titleStmt/tei:title[1]/text()"
+denormalize-indices -f "./data/editions/critical/*.xml" -i "./data/indices/*.xml" -m ".//*[@ref]/@ref | .//*/@source" -x ".//tei:titleStmt/tei:title[@type='manifestation']/text()"
 
 echo "done"
