15b328bb1859b9aa2c5e0922184d3bbc7b2dc04d
Daniel_Stoxreiter <daniel.stoxreiter@outlook.de>
2022-12-12 15:34:27 +0100

    initial commit ts

---
+++ html/js/ts_search.js
@@ -0,0 +1,196 @@
+const typesenseInstantsearchAdapter = new TypesenseInstantSearchAdapter({
+        server: {
+            apiKey: 'xyz', // Be sure to use an API key that only allows searches, in production
+            nodes: [
+                {
+                    host: '0.0.0.0',
+                    port: '8108',
+                    protocol: 'http'
+                }
+            ]
+        },
+        // The following parameters are directly passed to Typesense's search API endpoint.
+        //  So you can pass any parameters supported by the search endpoint below.
+        //  queryBy is required.
+        //  filterBy is managed and overridden by InstantSearch.js. To set it, you want to use one of the filter widgets like refinementList or use the `configure` widget.
+        additionalSearchParameters: {
+            queryBy: 'full_text'
+        }
+    });
+    const searchClient = typesenseInstantsearchAdapter.searchClient;
+    const search = instantsearch({
+        searchClient,
+        indexName: 'freud-edition'
+    });
+
+search.addWidgets([
+    instantsearch.widgets.searchBox({
+        container: '#searchbox',
+        autofocus: true,
+        cssClasses: {
+          form: 'form-inline',
+          input: 'form-control col-md-11',
+          submit: 'btn',
+          reset: 'btn'
+        }
+    }),
+    instantsearch.widgets.configure({
+        hitsPerPage: 8
+    }),
+    instantsearch.widgets.hits({
+        container: '#hits',
+        templates: {
+            empty: 'Keine Ergebnisse',
+            item: 
+                `
+                    <h4><a href="{{ id }}.html">{{ title }}</a></h4>
+                    <p style="overflow:hidden;max-height:410px;">{{#helpers.snippet}}{ "attribute": "full_text" }{{/helpers.snippet}}</p>
+                    <h5><span class="badge badge-primary">{{ project }}</span></h5>
+                    <div>
+                        <div>
+                            {{#persons}}
+                            <span class="badge badge-secondary">{{ . }}</span>
+                            {{/persons}}
+                        </div>
+                        <div>
+                            {{#works}}
+                            <span class="badge badge-success">{{ . }}</span>
+                            {{/works}}
+                        </div>
+                        <div>
+                            {{#keywords}}
+                            <span class="badge badge-info">{{ . }}</span>
+                            {{/keywords}}
+                        </div>
+                    </div>
+                `
+            }
+        }
+    ),
+
+    instantsearch.widgets.pagination({
+        container: '#pagination'
+    }),
+
+    instantsearch.widgets.stats({
+        container: '#stats-container',
+        templates: {
+          text: `
+            {{#areHitsSorted}}
+              {{#hasNoSortedResults}}Keine Treffer{{/hasNoSortedResults}}
+              {{#hasOneSortedResults}}1 Treffer{{/hasOneSortedResults}}
+              {{#hasManySortedResults}}{{#helpers.formatNumber}}{{nbSortedHits}}{{/helpers.formatNumber}} Treffer {{/hasManySortedResults}}
+              aus {{#helpers.formatNumber}}{{nbHits}}{{/helpers.formatNumber}}
+            {{/areHitsSorted}}
+            {{^areHitsSorted}}
+              {{#hasNoResults}}Keine Treffer{{/hasNoResults}}
+              {{#hasOneResult}}1 Treffer{{/hasOneResult}}
+              {{#hasManyResults}}{{#helpers.formatNumber}}{{nbHits}}{{/helpers.formatNumber}} Treffer{{/hasManyResults}}
+            {{/areHitsSorted}}
+            gefunden in {{processingTimeMS}}ms
+          `,
+        }
+    }),
+  
+    instantsearch.widgets.refinementList({
+        container: '#refinement-list-keywords',
+        attribute: 'keywords',
+        searchable: true,
+        searchablePlaceholder: 'Suche',
+        cssClasses: {
+        searchableInput: 'form-control form-control-sm mb-2 border-light-2',
+        searchableSubmit: 'd-none',
+        searchableReset: 'd-none',
+        showMore: 'btn btn-secondary btn-sm align-content-center',
+        list: 'list-unstyled',
+        count: 'badge ml-2 badge-info',
+        label: 'd-flex align-items-center text-capitalize',
+        checkbox: 'mr-2',
+        }
+    }),
+
+    instantsearch.widgets.refinementList({
+        container: '#refinement-list-persons',
+        attribute: 'persons',
+        searchable: true,
+        searchablePlaceholder: 'Suche',
+        cssClasses: {
+        searchableInput: 'form-control form-control-sm mb-2 border-light-2',
+        searchableSubmit: 'd-none',
+        searchableReset: 'd-none',
+        showMore: 'btn btn-secondary btn-sm align-content-center',
+        list: 'list-unstyled',
+        count: 'badge ml-2 badge-secondary',
+        label: 'd-flex align-items-center text-capitalize',
+        checkbox: 'mr-2',
+        }
+    }),
+
+    instantsearch.widgets.refinementList({
+        container: '#refinement-list-works',
+        attribute: 'works',
+        searchable: true,
+        searchablePlaceholder: 'Suche',
+        cssClasses: {
+        searchableInput: 'form-control form-control-sm mb-2 border-light-2',
+        searchableSubmit: 'd-none',
+        searchableReset: 'd-none',
+        showMore: 'btn btn-secondary btn-sm align-content-center',
+        list: 'list-unstyled',
+        count: 'badge ml-2 badge-success',
+        label: 'd-flex align-items-center text-capitalize',
+        checkbox: 'mr-2',
+        }
+    }),
+    
+    instantsearch.widgets.rangeInput({
+        container: "#range-input",
+        attribute: "year",
+        templates: {
+        separatorText: 'bis',
+        submitText: 'Suchen',
+        },
+        cssClasses: {
+        form: 'form-inline',
+        input: 'form-control',
+        submit: 'btn'
+        }
+    }),
+
+    instantsearch.widgets.pagination({
+        container: '#pagination',
+        padding: 2,
+        cssClasses: {
+        list: 'pagination',
+        item: 'page-item',
+        link: 'page-link'
+        }
+    }),
+    instantsearch.widgets.clearRefinements({
+        container: '#clear-refinements',
+        templates: {
+        resetLabel: 'Filter zurücksetzen',
+        },
+        cssClasses: {
+        button: 'btn'
+        }
+    }),
+
+        
+
+    instantsearch.widgets.currentRefinements({
+    container: '#current-refinements',
+    cssClasses: {
+        delete: 'btn',
+        label: 'badge'
+        }
+    })
+]);
+
+search.addWidgets([
+    instantsearch.widgets.configure({
+        attributesToSnippet: ['full_text'],
+    })
+]);
+
+search.start();
--- xslt/search.xsl
+++ xslt/search.xsl
@@ -21,21 +21,50 @@
                 <div class="hfeed site" id="page">
                     <xsl:call-template name="nav_bar"/>
                     
-                    <div class="container-fluid">
-                        <div class="card">
-                            <div class="card-header">
-                                <h1><xsl:value-of select="$doc_title"/></h1>
-                            </div>
-                            <div class="card-body">
-                                <div id="staticSearch"/>                            
+                    <header class="header">
+                        <h1 class="header-title">
+                            Volltextsuche
+                        </h1>
+                    </header>
+                    
+                    <div class="container-fluid" style="max-width:100%;">
+                        <div class="search-panel">
+                            <div class="search-panel__results">
+                                <div class="row">
+                                    <div class="col-md-4">
+                                        <div id="stats-container"></div>
+                                        <div id="searchbox"></div>
+                                        <div id="current-refinements"></div>
+                                        <div id="clear-refinements"></div>
+                                        <h4>Personen</h4>
+                                        <div id="refinement-list-persons"></div>
+                                        <h4>Schlagworte</h4>
+                                        <div id="refinement-list-keywords"></div>
+                                        <h4>Werke</h4>
+                                        <div id="refinement-list-works"></div>
+                                        <h4>Jahr</h4>
+                                        <div id="range-input"></div>
+                                    </div>
+                                    <div class="col-md-8">
+                                        <div id="pagination"></div>
+                                        <div id="hits"></div>
+                                    </div>
+                                </div>
                             </div>
                         </div>
+                        
+                        
                     </div>
                     
                     <xsl:call-template name="html_footer"/>
                     
                 </div>
             </body>
+            <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/instantsearch.css@7/themes/algolia-min.css"/>
+            <link rel="stylesheet" href="css/ts_search.css"/>
+            <script src="https://cdn.jsdelivr.net/npm/instantsearch.js@4.44.0"></script>
+            <script src="https://cdn.jsdelivr.net/npm/typesense-instantsearch-adapter@2/dist/typesense-instantsearch-adapter.min.js"></script>
+            <script src="js/ts_search.js"></script>            
         </html>
     </xsl:template>
 </xsl:stylesheet>
